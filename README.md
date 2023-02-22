ba5241542ac985629478392bb4b13326028cc9bc
John Whiles <johnwhiles@outlook.com>
2023-02-22 07:42:31 +0100

    iniitial commit

---
+++ README.md
@@ -0,0 +1,152 @@
+# Doctor Job 
+
+Doctor Job is built to allow you to easily create jobs as part of transactions in Prisma.
+It was somewhat inspired by the problems mentioned in [this article](https://brandur.org/job-drain).
+Those problems are that when we write data to a DB, we sometimes want to create a job associated with that data. 
+Naively we might try to write the data, then use that data to send a job to some external queue.
+However we can hit failure modes where the data is created, but the job is not queued, or the job is created, but references non existent data - even if we are using transactions.
+More details can be found in the article.
+
+We can get some more safety if we store the jobs to be queued in the same DB as data, and only later process send them to an external queue.
+This is because we can rely on transactions to ensure that creating data and jobs will always succeed or fail together.
+
+
+## Prerequisites
+You will need to use Prisma.. You will also need to enable interactive transactions by adding interactiveTransactions in the generator of your Prisma Schema:
+
+```
+generator client {
+  provider        = "prisma-client-js"
+  previewFeatures = ["interactiveTransactions"]
+}
+```
+
+You will also need a jobs table and a dead letter queue table. You have some leeway in how they are structured.
+Mine look something like this.
+
+```
+model Job {
+  id    String @id @default(cuid())
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  data String
+}
+
+model DeadLetters {
+  id    String @id @default(cuid())
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+
+  data String
+}
+``` 
+
+## Using It
+
+
+Here we set up a type for our jobs, and a function that actually handles them.
+In this case I've assumed we handle them directly in the same code base - but you could also
+just drain the jobs into an external job queue.
+```
+export type ParsedJob =
+  | {
+      type: "sendClientLoginLink";
+      options: {
+        clientId: string;
+      };
+    }
+  | {
+      type: "reticulateSplines";
+      options: {
+        target: string;
+      };
+    };
+
+export const handleParsedJobs = async (job: ParsedJob) => {
+  if (job.type === "sendClientLoginLink") {
+    const client = await getClientById(job.options.clientId);
+    invariant(client);
+
+    const loginToken = await createMagicLinkLoginToken(job.options.clientId);
+
+    sendClientLoginLinkEmail(client.email, client.id, loginToken);
+  } else if (job.type === "reticulateSplines") {
+    throw new Error("reticulateSplines is not implemented yet!");
+  } else {
+    throw new Error(`found an unknown job ${job}`);
+  }
+};
+```
+
+Here we set up a function that can take the data we have in Prisma
+and parse it into the format used by our function above
+```
+import * as E from "fp-ts/Either";
+import type { Job, DeadLetters } from "@prisma/client";
+
+const parseJob = (job: Job): E.Either<Error, ParsedJob> => {
+  // You might want to parse your data slightly better than this ;)
+  return E.right(JSON.parse(job.data));
+};
+```
+
+Then we can set up our instance of DoctorJob, with a couple more functions passed in 
+to handle things like creating and retrieving jobs and deadletters.
+```
+const doctorJob = new DoctorJob<ParsedJob, Job, DeadLetters>({
+  prismaClient,
+  parseJob,
+  getJob: async (client) => {
+    return await client.job.findFirst({
+      orderBy: { createdAt: "asc" },
+    });
+  },
+  createJob: async (tx, data) => {
+    await tx.job.create({
+      data: {
+        data,
+      },
+    });
+  },
+  getDeadLetters: async (client) => {
+    return client.deadLetters.findMany();
+  },
+  createDeadLetter: async (client, job) => {
+    await client.deadLetters.create({
+      data: {
+        id: job.id,
+        data: job.data,
+      },
+    });
+  },
+});
+```
+
+Then we can start running the `handleParsedJobs` function in a loop.
+
+```
+setInterval(() => doctorJob.run(handleParsedJobs), 1000);
+```
+
+Finally we can start adding jobs to the queue like so:
+```
+async function createLoginLinkForClient(email: string) {
+  return await doctorJob.queue(async () => {
+    const client = await prisma.client.findUniqueOrThrow({ where: { email } });
+    const job = {
+      type: "sendClientLoginLink" as const,
+      options: {
+        clientId: client.id,
+      },
+    };
+
+    return { data: null, job };
+  });
+}
+```
+
+## Disclaimer
+This is extracted from a personal project, has not been code reviewed, and almost certainly is not something you want to use.
---
+++ src/index.ts
@@ -0,0 +1,189 @@
+import type { Prisma, PrismaClient } from "@prisma/client";
+import * as E from "fp-ts/Either";
+import * as TE from "fp-ts/TaskEither";
+import { pipe, flow } from "fp-ts/lib/function";
+import type { Console } from 'node:console'
+
+export type TransactionClient = Prisma.TransactionClient;
+
+/**
+ * DoctorJobs lets you queue jobs as part of Prisma transactions. This helps you avoid two failure modes
+ * 1. The data is created, but a necessary job (eg, sending a confirmation email) is not queued.
+ * 2. The data fails to be created, but a job that would refer to that data is created anyway.
+ *
+ * Using DoctorJobs means that if the data exists, then so will the job - and vice versa
+ *
+ *
+ * @template Jobs - The type of job to be processed.
+ * @template Input - The type of data to be input to the job processing function.
+ * @template DeadLetters - The type of dead letter to be created in case of job processing failure.
+ */
+export class DoctorJobs<
+  Jobs,
+  Input extends { id: string; data: string },
+  DeadLetters
+> {
+  /**
+   * @type {PrismaClient}
+   */
+  client: PrismaClient;
+
+  /**
+   * A function for parsing input data into job data.
+   *
+   * @type {(input: Input) => E.Either<Error, Jobs>}
+   */
+  parseJob: (input: Input) => E.Either<Error, Jobs>;
+
+  /**
+   * A function for getting the next job from the queue.
+   *
+   * @type {() => Promise<Input>}
+   */
+  getJob: () => Promise<Input>;
+
+  /**
+   * A function for creating a new job in the queue.
+   *
+   * @type {(client: TransactionClient, data: string) => Promise<any>}
+   */
+  createJob: (client: TransactionClient, data: string) => Promise<any>;
+
+  /**
+   * A function for creating a new dead letter in case of job processing failure.
+   *
+   * @type {(input: DeadLetters) => Promise<void>}
+   */
+  createDeadLetter: (input: DeadLetters) => Promise<void>;
+
+  /**
+   * A function for getting all dead letters.
+   *
+   * @type {() => Promise<DeadLetters[]>}
+   */
+  getDeadLetters: () => Promise<DeadLetters[]>;
+
+  /**
+   * A logger for logging information about job processing.
+   *
+   * @type {Console}
+   */
+
+  log: any;
+
+  /**
+   * Creates a new `DoctorJobs` instance.
+   *
+   * @param {{
+   *   prismaClient: PrismaClient;
+   *   parseJob: (input: Input) => E.Either<Error, Jobs>;
+   *   getJob: (client: PrismaClient) => Promise<any>;
+   *   createJob: (client: TransactionClient, data: string) => Promise<void>;
+   *   createDeadLetter: (client: PrismaClient, input: DeadLetters) => Promise<void>;
+   *   getDeadLetters: (client: PrismaClient) => Promise<DeadLetters[]>;
+   *   logger?: Console;
+   * }} options - An object containing options for configuring the `DoctorJobs` instance.
+   */
+  constructor({
+    prismaClient,
+    parseJob,
+    getJob,
+    createJob,
+    createDeadLetter,
+    getDeadLetters,
+    logger
+  }: {
+    prismaClient: PrismaClient;
+    parseJob: (input: Input) => E.Either<Error, Jobs>;
+    getJob: (client: PrismaClient) => Promise<Input>;
+    createJob: (client: TransactionClient, data: string) => Promise<void>;
+    createDeadLetter: (
+      client: PrismaClient,
+      input: DeadLetters
+    ) => Promise<void>;
+    getDeadLetters: (client: PrismaClient) => Promise<DeadLetters[]>;
+    logger?: Console;
+  }) {
+    this.client = prismaClient;
+    this.parseJob = parseJob;
+    this.getJob = getJob.bind(this, this.client);
+    this.createJob = createJob;
+    this.createDeadLetter = createDeadLetter.bind(this, this.client);
+    this.getDeadLetters = getDeadLetters.bind(this, this.client);
+    this.log = logger 
+
+    // I'm binding this, because run eventually gets run in a setTimeout
+    // but needs refrence to `this`
+    this.run = this.run.bind(this);
+  }
+
+  /**
+   * Queues a new job for processing. It relies on Prisma's transaction client which must be passed by the caller.
+   * Using this client means that if any calls fail, then they will all fail - including the call to create the job.
+   *
+   * @param {(tx: TransactionClient) => Promise<{ data: Input; job: Jobs }>} fn - A function that takes the transaction client, returns the input data and job to be processed.
+
+  */
+  queue<Input>(
+    fn: (tx: TransactionClient) => Promise<{ data: Input; job: Jobs }>
+  ): Promise<Input> {
+    return this.client.$transaction(async (tx: TransactionClient) => {
+      const { data, job } = await fn(tx);
+
+      // save the stringified job.
+      await this.createJob(tx, JSON.stringify(job));
+
+      // Return the data, which can be used by the caller
+      return data;
+    });
+  }
+
+  /**
+   * Runs the next available job by finding it using getJob and parsing it with the provided parser.
+   * If a job is found, it is executed using the provided handleJobs function. If the job succeeds, it is deleted.
+   * If it fails, it is deleted from the job queue and added to the dead letter queue.
+   * @param {Function} handleJobs - A function that takes a job object and returns a Promise that resolves when the job is completed.
+   * @returns {Promise<void>} - A Promise that resolves with nothing when the function completes.
+   */
+  async run(handleJobs: (job: Jobs) => Promise<void>): Promise<void> {
+    this.log.info(`Attempting to find job`);
+    const job = await this.getJob();
+
+    if (job) {
+      this.log.info(`Running job with id ${job.id}`);
+      return pipe(
+        TE.fromEither(this.parseJob(job)),
+        flow(
+          TE.chain((parsed) =>
+            TE.tryCatch(
+              () => handleJobs(parsed),
+              (err) => new Error(`handling job failed: ${err}`)
+            )
+          ),
+          TE.fold(
+            // An error has been returned somewhere in the chain, so we delete the job
+            // and add it to the DLQ
+            (err) => async () => {
+              this.log.error(`An error occured: ${err}`);
+              await this.client.job.delete({ where: { id: job.id } });
+              await this.client.deadLetters.create({
+                data: {
+                  id: job.id,
+                  data: job.data,
+                },
+              });
+            },
+
+            // The job has succeeded
+            () => async () => {
+              this.log.info(`Finished processing job with id ${job.id}`);
+              await this.client.job.delete({ where: { id: job.id } });
+            }
+          )
+        )
+      )();
+    } else {
+      this.log.info("No job found");
+    }
+  }
+}
